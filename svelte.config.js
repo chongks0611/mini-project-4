bca895cfcb1ff9aa98f920bcb89e5c3aedfed676
chongks0611 <veralynchong@gmail.com>
2021-10-24 09:20:45 +0000

    Vercel deploy

--- svelte.config.js
+++ svelte.config.js
@@ -6,4 +6,14 @@ const config = {
 	}
 };
 
+
+import vercel from '@sveltejs/adapter-vercel';
+
+const config = {
+    kit: {
+   	 // hydrate the <div id="svelte"> element in src/app.html
+   	 target: '#svelte',
+   	 adapter: vercel()
+    }
+};
 export default config;
